49ca8b703471756c30fc0650aa6a1905390bb32a
Nour <almonzer1234567@gmail.com>
2025-08-15 11:15:38 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const App = () => {
+  return (
+    <main>
+      <div>
+        <h1 className="text-4xl font-bold text-pink">
+          Welcome to the world of GTA and GSAP
+        </h1>
+      </div>
+    </main>
+  );
+};
+export default App;
