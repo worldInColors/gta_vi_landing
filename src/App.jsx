c0bc3f7482215e91c2653a04a356cdaee6b91823
Nour <almonzer1234567@gmail.com>
2025-08-16 15:08:11 -0400

    Adds new section to main content layout
    Introduces an additional section to the page, expanding the showcased content and potentially enhancing user engagement by diversifying available information.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import NavBar from "./sections/NavBar";
 import FirstVideo from "./sections/FirstVideo";
 import Jason from "./sections/Jason";
 import SecondVideo from "./sections/SecondVideo";
+import Lucia from "./sections/Lucia";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -16,6 +17,7 @@ const App = () => {
       <FirstVideo />
       <Jason />
       <SecondVideo />
+      <Lucia />
     </main>
   );
 };
